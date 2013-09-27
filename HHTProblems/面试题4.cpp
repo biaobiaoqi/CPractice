2584c8f1cd43a93797ff68239dad87c373a08314
biaobiaoqi <shenyapeng@gmail.com>
2013-09-27 16:33:57 +0800

    Update 面试题4.cpp

--- HHTProblems/面试题4.cpp
+++ HHTProblems/面试题4.cpp
@@ -1,15 +1,41 @@
 #include <stdio.h>
-int ch;
+#include <vector>
+using namespace std;
+vector<char> v;
+
 int main()
 {
-	//freopen("G:\in.txt", "r", stdin);
+	freopen("G:\in.txt", "r", stdin);
+	char ch;
+	int length = 0;
+	int whiteSpaceLength = 0;
 	while(scanf("%c", &ch)!= EOF){
-		if (ch == ' '){
-			printf("%%20");
-		}else {
-			printf("%c", ch);
+		v.push_back(ch);
+		++ length;
+		if (ch == ' ') {
+			++ whiteSpaceLength;
 		}
 	}
-	//fclose(stdin);
+	v.push_back('\0');
+	++length;
+	v.resize(length + whiteSpaceLength*2);
+	
+	int originalIndex = length - 1;
+	int newIndex = length + whiteSpaceLength*2 - 1;
+	while (originalIndex >= 0 && originalIndex < newIndex) {
+		if (v[originalIndex] == ' ') {
+			v[newIndex --] = '0';
+			v[newIndex --] = '2';
+			v[newIndex --] = '%';
+		}else {
+			v[newIndex --] = v[originalIndex];
+		}
+		-- originalIndex;
+	}
+	
+	newIndex = 0;
+	while(v[newIndex]){
+		printf("%c",v[newIndex++]);	
+	}	
 	return 0;
 }

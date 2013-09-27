e367c4bef123e067fa755428f74462181d16ddbe
biaobiaoqi <shenyapeng@gmail.com>
2013-09-27 11:19:00 +0800

    Update 面试题3.cpp

--- HHTProblems/面试题3.cpp
+++ HHTProblems/面试题3.cpp
@@ -1,51 +1,41 @@
 #include <stdio.h>
-int ar[1001][1001];
-int t, m, n;
-bool res;
-void find(int a, int b, int c, int d)
-{
-	if (res)
-		return;
-	if (a > c || b > d)
-		return;
+int a[1005][1005];
 
-	int x = (a + c)/2;
-	int y = (b + d)/2;
-	
-	if (ar[x][y] == t){
-		res = true;
-	} else if (ar[x][y] < t) {
-		find(x + 1, y + 1, c, d);
-		find(a, y + 1, x, d);
-		find(x + 1, b, c, y);
-	} else {
-		find(a, b, x - 1, y - 1);
-		find(a, y, x - 1, d);
-		find(x, b, c, y - 1);
-	}
+int find (int row, int collumn, int target) {
+    bool find = false;
+    
+    if (row > 0 && collumn > 0 && a != NULL) {
+        int r = 0;
+        int c = collumn - 1;
+        while (r < row && collumn >= 0) {
+            if (a[r][c] == target) {
+                find = true;
+                break;
+            }else if (a[r][c] < target) {
+                ++ r;
+            }else {
+                -- c;
+            }
+        }
+    }
+    
+    return find;
 }
 
-int main()
-{
-	//freopen("G:\in.txt", "r", stdin);
-	//freopen("G:\out.txt", "w", stdout);
-	while(scanf("%d%d%d",&m, &n, &t) != EOF){
-		for (int i = 1; i <=m; ++ i) {
-			for (int j = 1; j <=n; ++ j){
-				scanf("%d", &ar[i][j]);
-			}
-		}
-		res = false;
-
-		find(1, 1, m, n);
-
-		if (res)
-			printf("Yes\n");
-		else
-			printf("No\n");
-	}
-
-	//fclose(stdin);
-	//fclose(stdout);
-	return 0;
+int main() {
+    int m, n, t;
+    while (scanf("%d%d", &m, &n) != EOF) {
+        scanf("%d", &t);
+        for (int i = 0; i != m; ++ i) {
+            for (int j = 0; j != n; ++ j) {
+                scanf("%d", &a[i][j]);
+            }
+        }
+        if (find(m, n, t)) {
+            printf("Yes\n");
+        }else {
+            printf("No\n");
+        }
+    }
+    return 0;
 }
